45db6b5f135f14b9958250d7736c05d489f1c086
tumeguyo <tumeguyo18@gmail.com>
2024-03-19 13:54:17 +0300

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { SearchComponent } from './Pages/search/search.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
+import { BookingsComponent } from './Pages/bookings/bookings.component';
+import { AdminComponent } from './Pages/admin/admin.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { TrainsComponent } from './Pages/trains/trains.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'bookings', component: BookingsComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'trains', component: TrainsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: PageNotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { SearchComponent } from './Pages/search/search.component';
+import { BookingsComponent } from './Pages/bookings/bookings.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
+import { AdminComponent } from './Pages/admin/admin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RegisterComponent } from './Pages/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './Pages/login/login.component';
+import { TrainsComponent } from './Pages/trains/trains.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    BookingsComponent,
+    PageNotFoundComponent,
+    AdminComponent,
+    RegisterComponent,
+    LoginComponent,
+    TrainsComponent,
+
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule, MatDialogModule, RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
